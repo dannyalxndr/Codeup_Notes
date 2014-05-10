a204e82ef92d2d593559df64ecd2d204add00ed0
Daniel Jimenez <danieljimenez@daniels-air.att.net>
2014-05-10 15:21:30 -0500

    Sending sandbox2.php file with weather if/elseif commands

---
+++ sandbox2.php
@@ -0,0 +1,21 @@
+<?php
+
+$beautiful = "The weather is beautiful!";
+$bad = "The weather is awful.";
+$windy = "It's windy outside.";
+$x = "windy";
+
+if ($x == "good")
+{
+	echo "$beautiful" . PHP_EOL;
+}
+elseif ($x == "bad") 
+{
+	echo "$bad" . PHP_EOL;
+}
+elseif ($x == "windy") 
+{
+	echo "$windy" . PHP_EOL;
+}
+
+
